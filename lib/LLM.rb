b9efe8892a61770f0d33754cf47fe52fd292f874
Antar Azri <azantar@proton.me>
2024-10-03 16:57:28 +0100

    feat: Initialize the adapter base class

--- lib/LLM.rb
+++ lib/LLM.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "LLM/version"
+require_relative "LLM/adapter"
 
 module LLM
   class Error < StandardError; end
---
+++ lib/LLM/adapter.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module LLM
+  class Adapter
+  end
+end
