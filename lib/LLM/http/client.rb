115551af1aac701d87caef41c80e25705edc451d
Antar Azri <azantar@proton.me>
2024-10-08 23:52:31 +0100

    Handle errors in the http client

--- lib/LLM/http/client.rb
+++ lib/LLM/http/client.rb
@@ -8,7 +8,19 @@ module LLM
         "Authorization" => "Bearer #{secret}"
       }
 
-      Net::HTTP.post(uri, payload.to_json, headers)
+      begin
+        response = Net::HTTP.post(uri, payload.to_json, headers)
+        response.value
+        response
+      rescue Net::HTTPUnauthorized, Net::HTTPClientException
+        raise LLM::AuthError
+      rescue SystemCallError
+        raise LLM::NetError
+      rescue JSON::ParserError
+        raise LLM::ParseError
+      rescue => e
+        raise LLM::Error, "Unexpected Error: #{e.message}"
+      end
     end
   end
 end
