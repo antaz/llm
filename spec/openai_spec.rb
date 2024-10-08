12e3bc81e417e834ab5760330db2bfaadfc73615
Antar Azri <azantar@proton.me>
2024-10-08 12:23:25 +0100

    refactor: Extract `stub_request`s from `it` hooks

--- spec/openai_spec.rb
+++ spec/openai_spec.rb
@@ -5,28 +5,34 @@ require "webmock/rspec"
 RSpec.describe LLM::OpenAI do
   subject(:openai) { LLM::OpenAI.new("") }
 
-  it "Returns a successful completion" do
+  before(:each, :success) do
     stub_request(:post, "https://api.openai.com/v1/chat/completions")
       .to_return(
         status: 200,
         body: File.read(File.join(File.dirname(__FILE__), "fixtures", "openai_success.json")),
         headers: {"Content-Type" => "application/json"}
       )
-    expect(openai.complete("Hello!")).to eq([
-      {
-        role: "assistant",
-        message: "Hello! How can I assist you today?"
-      }
-    ])
   end
 
-  it "Returns an authentication error" do
+  before(:each, :auth_error) do
     stub_request(:post, "https://api.openai.com/v1/chat/completions")
       .to_return(
         status: 401,
         body: File.read(File.join(File.dirname(__FILE__), "fixtures", "openai_keyerror.json")),
         headers: {"Content-Type" => "application/json"}
       )
+  end
+
+  it "Returns a successful completion", :success do
+    expect(openai.complete("Hello!")).to match([
+      have_attributes(
+        role: "assistant",
+        message: "Hello! How can I assist you today?"
+      )
+    ])
+  end
+
+  it "Returns an authentication error", :auth_error do
     expect { openai.complete("Hello!") }.to raise_error(LLM::AuthError)
   end
 end
