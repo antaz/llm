94c50dc954ab8547b0b45b94cd7d6d8df621b8dd
0x1eef <0x1eef@protonmail.com>
2024-10-30 10:37:20 -0300

    Add 'conversation' API

--- lib/llm/providers/anthropic.rb
+++ lib/llm/providers/anthropic.rb
@@ -39,14 +39,14 @@ module LLM
     ##
     # @param (see LLM::Provider#completion_model)
     # @return (see LLM::Provider#completion_model)
-    def completion_model(raw)
+    def completion_model(_completion, raw)
       raw["model"]
     end
 
     ##
     # @param (see LLM::Provider#completion_messages)
     # @return (see LLM::Provider#completion_messages)
-    def completion_messages(raw)
+    def completion_messages(_completion, raw)
       raw["content"].map { LLM::Message.new("assistant", _1["text"]) }
     end
 
