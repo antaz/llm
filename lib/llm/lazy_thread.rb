c25cf776f9ae28733e668f433c158ec456df7a0d
0x1eef <0x1eef@protonmail.com>
2024-11-05 19:39:44 -0300

    rake standard:fix

--- lib/llm/lazy_thread.rb
+++ lib/llm/lazy_thread.rb
@@ -33,9 +33,9 @@ module LLM
 
     def complete!
       prompt, role, params = @thread[-1]
-      rest = @thread[0..-2].map { Array === _1 ? LLM::Message.new(_1[1], _1[0]) : _1 }
+      rest = @thread[0..-2].map { (Array === _1) ? LLM::Message.new(_1[1], _1[0]) : _1 }
       comp = @provider.complete(prompt, role, **params.merge(messages: rest)).choices.last
-      [ *rest, LLM::Message.new(role, prompt), comp ]
+      [*rest, LLM::Message.new(role, prompt), comp]
     end
   end
 end
