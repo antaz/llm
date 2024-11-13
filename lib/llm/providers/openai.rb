2ce2a6c129567ff838be4276f15d77b018400c30
Antar Azri <azantar@proton.me>
2024-11-13 16:12:25 +0100

    feat: add vision edits and variations

--- lib/llm/providers/openai.rb
+++ lib/llm/providers/openai.rb
@@ -35,12 +35,14 @@ module LLM
       Response::Completion.new(res.body, self).extend(response_parser)
     end
 
-    def vision(prompt, **params)
-      req = Net::HTTP::Post.new ["/v1", "images", "generations"].join("/")
-      body = {prompt:, model: "dall-e-3", n: 1}.merge!(params)
-      req = preflight(req, body)
-      res = request @http, req
-      Response::Vision.new(res.body, self).extend(response_parser)
+    %w[generation edit variation].each do |action|
+      define_method "vision_#{action}" do |prompt, **params|
+        req = Net::HTTP::Post.new ["/v1", "images", "#{action}s"].join("/")
+        body = {prompt:, model: "dall-e-3", n: 1}.merge!(params)
+        req = preflight(req, body)
+        res = request @http, req
+        Response::Vision.new(res.body, self).extend(response_parser)
+      end
     end
 
     ##
