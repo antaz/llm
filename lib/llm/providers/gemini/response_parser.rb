291662977de3f3cc7de9bc82a5805b6da16919f3
Antar Azri <azantar@proton.me>
2024-11-20 13:18:56 +0100

    feat: add `stop_reason` for Gemini completion Message

--- lib/llm/providers/gemini/response_parser.rb
+++ lib/llm/providers/gemini/response_parser.rb
@@ -16,9 +16,11 @@ class LLM::Gemini
       {
         model: raw["modelVersion"],
         choices: raw["candidates"].map do
+          stop_reason = _1["finishReason"]
           LLM::Message.new(
             _1.dig("content", "role"),
-            {text: _1.dig("content", "parts", 0, "text")}
+            {text: _1.dig("content", "parts", 0, "text")},
+            {stop_reason:}
           )
         end,
         prompt_tokens: raw.dig("usageMetadata", "promptTokenCount"),
