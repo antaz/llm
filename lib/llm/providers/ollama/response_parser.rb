25ff191213af726efbc5759b0cc7ac7cddca8b10
Antar Azri <azantar@proton.me>
2024-11-20 13:41:38 +0100

    feat: add `stop_reason` for Ollama completion message
    Note that for some reason Ollama does not always respond with `done_reason`.
    So that needs to be investigated further

--- lib/llm/providers/ollama/response_parser.rb
+++ lib/llm/providers/ollama/response_parser.rb
@@ -7,7 +7,7 @@ class LLM::Ollama
     def parse_completion(raw)
       {
         model: raw["model"],
-        choices: [LLM::Message.new(*raw["message"].values_at("role", "content"))],
+        choices: [LLM::Message.new(*raw["message"].values_at("role", "content"), {stop_reason: raw["done_reason"]})],
         prompt_tokens: raw.dig("prompt_eval_count"),
         completion_tokens: raw.dig("eval_count")
       }
